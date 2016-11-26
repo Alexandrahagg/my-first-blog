c4e48062e0e1bfbcc0d8c61e19c622fc45d338cf
Alexandra <alexandra@hotmail.com>
2016-11-26 11:43:06 +0100

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+#importerar modeller för att kunna definera hur ens data ser ut
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User') #this is a link to another model
+    title = models.CharField(max_length=200)   #this is how you define text with a limited number of characters
+    text = models.TextField() #this is for long text without a limit
+    created_date = models.DateTimeField(default=timezone.now) #this is a date and time
+    publiched_date = models.DateTimeField(blank=True, null=True)
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
---
+++ pythonintro.py
@@ -0,0 +1,5 @@
+
+if 5>2:
+	print('5 är större än 2')
+else:
+	print ('5 är inte större än 2')
